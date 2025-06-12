c3f9fb29dc372ea760888cb3d3322bd236862c14
Mohamed Alaa <mohamedalaasalem1@gmail.com>
2025-06-12 16:10:30 +0300

    Add Central SDK Manager

---
+++ Assets/Gensis/ARK_SDK/Runtime/ARKSDK.cs
@@ -0,0 +1,29 @@
+using ARK.SDK.Controllers;
+using ARK.SDK.Core;
+using ARK.SDK.Services;
+using UnityEngine;
+
+namespace ARK.SDK
+{
+    public static class ARKSDK
+    {
+        public static bool IsInitialized { get; private set; }
+
+        public static GraphQLClient Client { get; private set; }
+        public static AuthController Auth { get; private set; }
+
+        public static void Initialize(string endpoint)
+        {
+            if (IsInitialized)
+            {
+                Debug.Log("ARKSDK is already initialized.");
+                return;
+            }
+            Client = new GraphQLClient(endpoint);
+            var authService = new AuthService(Client);
+            Auth = new AuthController(authService);
+
+            IsInitialized = true;
+        }
+    }
+}
