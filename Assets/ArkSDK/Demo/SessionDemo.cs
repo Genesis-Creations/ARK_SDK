f1d4a96e878b64a0cf43147efcbcb3b14de098f7
Mohamed Alaa <mohamedalaasalem1@gmail.com>
2025-06-30 17:54:34 +0300

    Add update session to session demo

--- Assets/ArkSDK/Demo/SessionDemo.cs
+++ Assets/ArkSDK/Demo/SessionDemo.cs
@@ -1,5 +1,7 @@
 using ARK.SDK.Core;
+using ARK.SDK.Models.Session;
 using Newtonsoft.Json;
+using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
@@ -16,16 +18,21 @@ namespace ARK.SDK.Demo
 
         public TMP_InputField sessionIdInputField;
 
+        [Header("UpdateUserSession")]
+        public Button UpdateUserSessionButton;
+
         private void OnEnable()
         {
             GetActiveUserSessionButton.onClick.AddListener(GetActiveUserSession);
             StartUserSessionButton.onClick.AddListener(StartUserSession);
+            UpdateUserSessionButton.onClick.AddListener(UpdateUserSession);
         }
 
         private void OnDisable()
         {
             GetActiveUserSessionButton.onClick.RemoveListener(GetActiveUserSession);
             StartUserSessionButton.onClick.RemoveListener(StartUserSession);
+            UpdateUserSessionButton.onClick.RemoveListener(UpdateUserSession);
         }
 
         private async void GetActiveUserSession()
@@ -37,6 +44,56 @@ namespace ARK.SDK.Demo
         private async void StartUserSession()
         {
             await ARKManager.Session.StartUserSessionAsync(sessionIdInputField.text);
+        }
+
+        [ContextMenu("UpdateUserSession")]
+        public async void UpdateUserSession()
+        {
+            var moduleResultData = new ModuleResultInput
+            {
+                Id = "module123",
+                Duration = 10,
+                Interactions = new InteractionResultData[]
+    {
+        new InteractionResultData
+        {
+            Id = "interaction123",
+            Completed = 1.0f,
+            CompletionRatio = 1.0f,
+            Compliance = 1.0f,
+            ComplianceRatio = 1.0f,
+            Duration = 10,
+            Score = 100.0f,
+            Steps = new List<StepResultData>
+            {
+                new StepResultData
+                {
+                    Completed = true,
+                    Compliance = true,
+                    Title = "Step 1",
+                },
+                new StepResultData
+                {
+                    Completed = true,
+                    Compliance = true,
+                    Title = "Step 2",
+                }
+            },
+            Success = true,
+            Total = 100.0f
+        }
+    },
+            };
+            var userSessionResultInput = new UserSessionResultInput(moduleResultData);
+
+            if(ARKCache.Session == null || string.IsNullOrEmpty(ARKCache.Session.Id))
+            {
+                Debug.LogError("Session ID is not available. Please start a session first.");
+                return;
+            }
+
+            bool result = await ARKManager.Session.UpdateUserSessionAsync(ARKCache.Session.Id, userSessionResultInput);
+            Debug.Log($"Update User Session Result: {result}");
         }
     }
 }
