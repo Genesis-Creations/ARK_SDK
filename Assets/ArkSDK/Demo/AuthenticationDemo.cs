4a9dd3c2a8f8717d7bcbb20569cb9f9aa8ac7ebd
Mohamed Alaa <mohamedalaasalem1@gmail.com>
2025-06-30 13:45:49 +0300

    Add demo for authentication

---
+++ Assets/ArkSDK/Demo/AuthenticationDemo.cs
@@ -0,0 +1,43 @@
+using ARK.SDK.Core;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ARK.SDK.Demo
+{
+    public class AuthenticationDemo : MonoBehaviour
+    {
+        [Header("LoginWithMailAndPassword")]
+        public Button loginWithEmailAndPasswordButton;
+
+        public TMP_InputField emailInputField;
+        public TMP_InputField passwordInputField;
+
+        [Header("LoginWithPincode")]
+        public Button loginWithPincodeButton;
+
+        public TMP_InputField pinCodeInputField;
+
+        private void OnEnable()
+        {
+            loginWithEmailAndPasswordButton.onClick.AddListener(LoginWithEmailAndPassword);
+            loginWithPincodeButton.onClick.AddListener(LoginWithPinCode);
+        }
+
+        private void OnDisable()
+        {
+            loginWithEmailAndPasswordButton.onClick.RemoveListener(LoginWithEmailAndPassword);
+            loginWithPincodeButton.onClick.RemoveListener(LoginWithPinCode);
+        }
+
+        private async void LoginWithPinCode()
+        {
+            await ARKManager.Auth.LoginWithPinCodeAsync(pinCodeInputField.text);
+        }
+
+        private async void LoginWithEmailAndPassword()
+        {
+            await ARKManager.Auth.LoginWithMailAndPassowrdAsync(emailInputField.text, passwordInputField.text);
+        }
+    }
+}
